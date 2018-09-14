c3508b086e307f54d0c21a9beb27e3c29bf035e9
ezemp <ericzemp@gmail.com>
2018-09-13 21:28:04 -0600

    we are somewhat closer

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -91,10 +91,11 @@ exports.petfind = function(req, res) {
   var queryURL = 'http://api.petfinder.com/shelter.find?key=6c9f4c0537e24d967a967ac2ed603f91&location=' +zipcode+"&count=10";
   request(queryURL, function (error, response, body) {
   console.log("error", error)
-    console.log("stausCode", response&&response.statusCode )
-    console.log('body:', body)
+    // console.log("stausCode", response&&response.statusCode )
+    console.log('body:', body.shelter.latitude)
+    console.log("body", body.shelter.longitude)
   })
-  res.render("resources", hbsObject);
+  // res.render("resources", hbsObject);
 };
 // exports.update = function(req, res) {
 //   var hbsObject = {
